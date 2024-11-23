36023b873ba09cec0dab511c46df64dfd4efd522
Srithan M.S <123616762+srithan09@users.noreply.github.com>
2024-11-23 21:16:43 +0530

    folder structure completed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages , setup
+
+setup(
+    name= 'Medical Chatbot using LLM',
+    version='0.0.0',
+    author='Srithan M.S',
+    author_email='sritan09004@gmail.com',
+    packages=find_packages(),
+    install_requires =[]
+)
